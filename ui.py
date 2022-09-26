ac69a517c424098fe9d00ede01484a9ecc4a251f
juanburdick <juanburdick@outlook.com>
2022-09-25 21:58:35 -0400

    init also import is being a pain

---
+++ ui.py
@@ -0,0 +1,17 @@
+'''Create the startup window.'''
+# pylint: disable=fixme
+import sys
+from PyQ5.QtWidgets import QApplication, QMainWindow
+
+class Game(QMainWindow):
+    '''The main game window'''
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Game')
+        self.setCentralWidget(self.window())
+
+if __name__ == '__main__':
+    app = QApplication([])
+    game = Game()
+    game.show()
+    sys.exit(app.exec_())
