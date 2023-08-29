352b353b21e5a68565be02dd25032c104d2cc804
juanburdick <juanburdick@outlook.com>
2023-08-29 10:52:53 -0400

    remove confusing inheritance, didnt add much

--- tabs/game.py
+++ tabs/game.py
@@ -30,30 +30,31 @@ class GameTab(QWidget):
         meta = MetaBoard(self, 700, (0,0,1,1))
         self.layout().addWidget(meta.groupbox, *meta.layout_position) # type: ignore
 
-class Board(QWidget):
+class MetaBoard(QWidget):
+    '''Contain and track the main game/game board'''
     GRID = [(0,0,1,1), (0,1,1,1), (0,2,1,1),
             (1,0,1,1), (1,1,1,1), (1,2,1,1),
             (2,0,1,1), (2,1,1,1), (2,2,1,1)]
 
-    '''Format for a tic tac toe game, meta or subgame'''
-    def __init__(self, parent: Union[GameTab, 'MetaBoard'], size: int, layout_position : Tuple[int,...] = (0,0)):
+    def __init__(self, parent: GameTab, size: int, layout_position: Tuple[int,...] = (0,0)):
         super().__init__(parent)
         self.groupbox = GameGroupBox(GROUPBOX_STYLE, size)
         self.layout_position = layout_position
-
-class MetaBoard(Board):
-    '''Contain and track the main game/game board'''
-    def __init__(self, parent: GameTab, size: int, layout_position: Tuple[int,...] = (0,0)):
-        super().__init__(parent, size, layout_position)
 
         for position in self.GRID:
             subgame = SubGameBoard(self, 220, position)
             self.groupbox.layout().addWidget(subgame.groupbox, *subgame.layout_position) # type: ignore
 
-class SubGameBoard(Board):
+class SubGameBoard(QWidget):
     '''Contains a subgame of tic tac toe'''
+    GRID = [(0,0,1,1), (0,1,1,1), (0,2,1,1),
+            (1,0,1,1), (1,1,1,1), (1,2,1,1),
+            (2,0,1,1), (2,1,1,1), (2,2,1,1)]
+
     def __init__(self, parent: MetaBoard, size: int, layout_position : Tuple[int,...] = (0,0)):
-        super().__init__(parent, size, layout_position)
+        super().__init__(parent)
+        self.groupbox = GameGroupBox(GROUPBOX_STYLE, size)
+        self.layout_position = layout_position
 
         for position in self.GRID:
             button = GameButton(f'{position[:2]}', 60, position)
