f343b43da0f3cea5c15fc89e44c55a071d91674e
juanburdick <juanburdick@outlook.com>
2023-09-26 16:06:34 -0400

    get the new state handler working
    - update boards now recognizes claimed boards, and can activate all boards if target is claimed
    - switch turn was unnecessary, the various methods that called it can simply cycle the turn and call the indicator update themselves
    - whose_turn replaced by take_turn, where the actions taken are better labeled
    - undo last move is also better explained
    - GameState class removed. reverting the turn only requires resetting the button and redrawing the board as if the previous button was selected
    - rewrote GameButton.claim() so it is more explicit that self.gc.take_turn() alters the game state
    - added is_claimed property to SubGameBoard and made it update appropriately

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 '''Create the startup window.'''
 # pylint: disable=no-name-in-module, import-error, fixme
 import sys
-from enum import Enum, auto
+from enum import Enum
 from typing import List, Dict, Tuple, Optional
 from itertools import product, cycle
 import numpy as np
@@ -17,8 +17,8 @@ _CLAIMED_SIZE = 195
 
 class TURN(Enum):
     '''Enum for tracking turn player'''
-    PL_1 = auto()
-    PL_2 = auto()
+    PL_1 = 1
+    PL_2 = 2
 
 TURN_CYCLER = cycle((TURN.PL_1, TURN.PL_2))
 
@@ -41,33 +41,44 @@ class GameController:
         return None
 
     def update_boards(self, target: Tuple[int,int]):
-        '''Set boards active or inactive based on the last move'''
-        for layout_position, board in self.boards.items():
-            if layout_position == target:
-                board.set_active_board()
-            else:
-                board.disable_board()
+        '''Try to activate a board (if it's already claimed, activate all unclaimed boards instead)'''
+        target_board = self.boards[target]
 
-    def switch_turn(self, source_button: Optional['GameButton'] = None):
-        if source_button is not None:
-            self.update_boards(source_button.layout_position)
+        if target_board.is_claimed: # if target board is claimed, enable all other unclaimed boards
+            for board in self.boards.values():
+                if not board.is_claimed:
+                    board.set_active_board()
+
+        else: # if target board isn't claimed, enable target and disable all other unclaimed boards
+            target_board.set_active_board()
+            for board in self.boards.values():
+                if not board.is_claimed and board is not target_board:
+                    board.disable_board()
+
+    def take_turn(self, button: 'GameButton') -> TURN:
+        '''method that returns active player's turn, stores turn history, and updates turn player'''
+        self.update_boards(target = button.layout_position)
+        self.turn_history.append(button)
+
+        player = self.turn
         self.turn = next(TURN_CYCLER)
         self.parent.update_turn_indicators()
-
-    def whose_turn(self, button: 'GameButton') -> TURN:
-        '''method that returns active player's turn, stores turn history, and updates turn player'''
-        ret = self.turn
-        self.switch_turn(button)
-        self.turn_history.append(button)
-        return ret
+        return player
 
     def undo_last_move(self):
         '''undo the most recent move'''
         if len(self.turn_history) > 1:
-            self.turn_history.pop().resetButtonstate()
-            self.switch_turn(self.turn_history[-1])
-        else:
-            self.reset_new_game()
+            square_played = self.turn_history.pop() # remove the last play from the history to discard it
+            square_played.resetButtonstate() # reset the button before discarding the play
+
+            prev_square = self.turn_history[-1] # to reset the board, we need to act as if the prev turn was just taken
+            self.update_boards(target = prev_square.layout_position)
+
+            self.turn = next(TURN_CYCLER) # since the game is binary, we can simply progress the turn cycler
+            self.parent.update_turn_indicators() # and update the turn indicators appropriately
+
+        else: # since undoing carries a backref to the prev turn, if the turn to be undone was the first one
+            self.reset_new_game() # just reset
 
     def reset_new_game(self):
         '''reset the board for a new game'''
@@ -76,15 +87,9 @@ class GameController:
                 button.resetButtonstate()
             board.set_active_board()
         if self.turn is TURN.PL_2:
-            self.switch_turn()
+            self.turn = next(TURN_CYCLER)
+            self.parent.update_turn_indicators()
         self.turn_history.clear()
-
-class GameState:
-    '''class representing a frame of the game state, used for turn history'''
-    def __init__(self, player: TURN, board: 'SubGameBoard', button: 'GameButton') -> None:
-        self.turn_player = player
-        self.board_played = board
-        self.square_played = button
 
 class GameButton(QPushButton):
     """A standardized button for the game"""
@@ -105,7 +110,8 @@ class GameButton(QPushButton):
     def claim(self):
         '''a user has selected this square, gaining control of it'''
         self.is_claimed = True
-        ref = SELECT.PL_1 if self.gc.whose_turn(self) is TURN.PL_1 else SELECT.PL_2
+        turn = self.gc.take_turn(self)
+        ref = SELECT.PL_1 if turn is TURN.PL_1 else SELECT.PL_2
         self.setStyleSheet(get_btn_style(ref))
         self.setDisabled(True)
 
@@ -144,6 +150,7 @@ class SubGameBoard(QWidget):
         self.gc = game_controller
         self.groupbox = GameGroupBox(get_box_style(SELECT.DEFAULT_BOX), size, size)
         self.layout_position = layout_position
+        self.is_claimed: bool = False
 
         self.buttons: List[GameButton] = []
         self.claimed_button = GameButton(self, self.gc, 'x', _CLAIMED_SIZE, (0,0))
@@ -176,6 +183,7 @@ class SubGameBoard(QWidget):
 
     def claim_board(self):
         '''a player has won this board, display that player's symbol and lock the board'''
+        self.is_claimed = True
         for button in self.buttons:
             button.setParent(None)
 
@@ -186,6 +194,7 @@ class SubGameBoard(QWidget):
 
     def reset_board(self):
         '''reset the board'''
+        self.is_claimed = False
         self.claimed_button.setParent(None)
         for button in self.buttons:
             button.setParent(self)
