4441eaa65a50775f28b3702fbd1c24e37a1c0d13
juanburdick <juanburdick@outlook.com>
2023-11-15 12:22:51 -0500

    anonymized turn indicators, but argument lists are getting unwieldy...I should generalize the registration method

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@
 # pylint: disable=no-name-in-module, import-error, fixme
 import sys
 from enum import Enum
-from typing import List, Dict, Tuple, Optional, Callable
+from typing import List, Dict, Tuple, Optional, Callable, Protocol
 from itertools import product, cycle
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QGridLayout, QWidget, QMainWindow, QPushButton
@@ -15,12 +15,16 @@ _BOX_SIZE = 220
 _BTN_SIZE = 60
 _CLAIMED_SIZE = 195
 
+class TurnIndicator(Protocol):
+    def update_turn_indicator(self, _: bool) -> None:
+        return
+
 class GameController:
     '''class for mainting the game data'''
-    def __init__(self, parent: 'GameWidget') -> None:
-        self.parent = parent
+    def __init__(self) -> None:
         self.turn = next(TURN_CYCLER)
         self.turn_history: List[GameButton] = []
+        self.turn_indicators: List[TurnIndicator] = []
         self.boards: Dict[Tuple[int,...], SubGameBoard] = {}
         self.buttons: np.ndarray[GameButton] = np.empty((3,3,3,3), dtype = GameButton) # type: ignore
 
@@ -31,6 +35,15 @@ class GameController:
     def register_button(self, board_position: Tuple[int,...], button: Optional[GameButton]):
         '''allows game pieces to register themself with the game controller'''
         self.buttons[board_position][button.layout_position] = button
+
+    def register_turn_indicator(self, indicator: TurnIndicator):
+        '''allows game pieces to register themself with the game controller'''
+        self.turn_indicators.append(indicator)
+
+    def update_turn_indicators(self):
+        '''Call all registered turn indicators to update their displays'''
+        for indicator in self.turn_indicators:
+            indicator.update_turn_indicator(self.turn is TURN.PL_1)
 
     def update_boards(self, target: Tuple[int,...]):
         '''Try to activate a board (if it's already claimed, activate all unclaimed boards instead)'''
@@ -54,7 +67,7 @@ class GameController:
 
         player = self.turn
         self.turn = next(TURN_CYCLER)
-        self.parent.update_turn_indicators()
+        self.update_turn_indicators()
         return player
 
     def undo_last_move(self):
@@ -67,7 +80,7 @@ class GameController:
             self.update_boards(target = prev_square.layout_position)
 
             self.turn = next(TURN_CYCLER) # since the game is binary, we can simply progress the turn cycler
-            self.parent.update_turn_indicators() # and update the turn indicators appropriately
+            self.update_turn_indicators() # and update the turn indicators appropriately
 
         else: # since undoing carries a backref to the prev turn, if the turn to be undone was the first one
             self.reset_new_game() # just reset
@@ -80,7 +93,7 @@ class GameController:
             board.set_active_board()
         if self.turn is TURN.PL_2:
             self.turn = next(TURN_CYCLER)
-            self.parent.update_turn_indicators()
+            self.update_turn_indicators()
         self.turn_history.clear()
 
 class MetaBoard(QWidget):
@@ -88,6 +101,7 @@ class MetaBoard(QWidget):
     def __init__(self,
                  size: int,
                  layout_position: Tuple[int,...],
+                 registration: Callable[[TurnIndicator], None],
                  board_registry: Callable[['SubGameBoard'], None],
                  button_callables: Tuple[Callable[[GameButton], TURN],
                                          Callable[[Tuple[int,...],GameButton],None]],
@@ -99,6 +113,8 @@ class MetaBoard(QWidget):
         for position in product((0,1,2), repeat = 2):
             subgame = SubGameBoard(_BOX_SIZE, position, board_registry, button_callables)
             self.groupbox.layout().addWidget(subgame.groupbox, *subgame.layout_position) # type: ignore
+
+        registration(self)
 
     def update_turn_indicator(self, is_player_one: bool):
         '''update the color of the groupbox to indicate which player's turn it is'''
@@ -171,26 +187,30 @@ class GameWidget(QWidget):
     '''Used to implement a tabbed system of splitting widgets'''
     def __init__(self, parent: 'HostWindow'):
         super().__init__(parent)
-        self.ancestor = parent
+
+        self.gc = GameController()
+
+        self.meta = MetaBoard(size = _GAME_SIZE,
+                              layout_position = (0,0,1,1),
+                              registration = self.gc.register_turn_indicator,
+                              board_registry = self.gc.register_board,
+                              button_callables = (self.gc.take_turn, self.gc.register_button))
+
+        self.menu = MenuBox(layout_position = (1,0,1,1),
+                            registration = self.gc.register_turn_indicator,
+                            new_game_method = self.gc.reset_new_game,
+                            undo_method = self.gc.undo_last_move,
+                            exit_method = parent.close)
+
         self.setLayout(QGridLayout())
-
-        self.gc = GameController(self)
-
-        self.meta = MetaBoard(_GAME_SIZE, (0,0,1,1), self.gc.register_board, (self.gc.take_turn, self.gc.register_button))
-        self.menu = MenuBox((1,0,1,1), self.gc.reset_new_game, self.gc.undo_last_move, self.ancestor.close)
-
         self.layout().addWidget(self.meta.groupbox, *self.meta.layout_position)
         self.layout().addWidget(self.menu.groupbox, *self.menu.layout_position)
-
-    def update_turn_indicators(self):
-        '''update menu and board turn indicators'''
-        self.menu.update_turn_indicator(self.gc.turn is TURN.PL_1)
-        self.meta.update_turn_indicator(self.gc.turn is TURN.PL_1)
 
 class MenuBox(QWidget):
     '''Parent class for GroupBoxes for jogging buttons'''
     def __init__(self,
                  layout_position: Tuple[int,...],
+                 registration: Callable[[TurnIndicator], None],
                  new_game_method: Callable,
                  undo_method: Callable,
                  exit_method: Callable,
@@ -210,6 +230,8 @@ class MenuBox(QWidget):
         exit_button = MenuButton("Exit Game", (0,2,1,1))
         exit_button.clicked.connect(exit_method)
         self.groupbox.layout().addWidget(exit_button, *exit_button.layout_position)
+
+        registration(self)
 
     def update_turn_indicator(self, is_player_one: bool):
         '''update the color of the groupbox to indicate which player's turn it is'''
